5406d02915413ff0189d402353119e5c87a5a9f3
Ervis Trupja <ervis@bettertrucks.com>
2024-11-09 17:01:00 +0100

    load posts with views

--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using CircleApp.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CircleApp.Controllers
@@ -6,14 +8,20 @@ namespace CircleApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var allPosts = await _context.Posts
+                .Include(n => n.User)
+                .ToListAsync();
+
             return View();
         }
     }
