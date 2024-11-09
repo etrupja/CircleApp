35aff82560dc01117b35dd0acb32c0d1e3a5c37a
Ervis Trupja <ervis@bettertrucks.com>
2024-11-09 22:26:35 +0100

    added like to post

--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -22,6 +22,7 @@ namespace CircleApp.Controllers
         {
             var allPosts = await _context.Posts
                 .Include(n => n.User)
+                .Include(n => n.Likes)
                 .OrderByDescending(n => n.DateCreated)
                 .ToListAsync();
 
