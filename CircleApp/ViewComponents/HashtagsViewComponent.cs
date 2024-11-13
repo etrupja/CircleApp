3d2527acce22913e01df1d99b19c86fac0e25665
Ervis Trupja <ervis@bettertrucks.com>
2024-11-13 22:38:26 +0100

    update view componnt with right data

--- CircleApp/ViewComponents/HashtagsViewComponent.cs
+++ CircleApp/ViewComponents/HashtagsViewComponent.cs
@@ -1,12 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CircleApp.ViewComponents
 {
     public class HashtagsViewComponent: ViewComponent
     {
+        private readonly AppDbContext _context;
+        public HashtagsViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            return View();
+            var oneWeekAgoNow = DateTime.UtcNow.AddDays(-7);
+
+            var top3Hashtags = await _context.Hashtags
+                .Where(h => h.DateCreated >= oneWeekAgoNow)
+                .OrderByDescending(n => n.Count)
+                .Take(3)
+                .ToListAsync();
+
+            return View(top3Hashtags);
         }
     }
 }
