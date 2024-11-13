af34dbc9c4ec41583189e8b8df5f05e95e7fbfe8
Ervis Trupja <ervis@bettertrucks.com>
2024-11-13 20:37:44 +0100

    load stories with view component

---
+++ CircleApp/ViewComponents/StoriesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CircleApp.ViewComponents
+{
+    public class StoriesViewComponent:ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public StoriesViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var allStories = await _context.Stories.Include(s => s.User).ToListAsync();
+            return View(allStories);
+        }
+    }
+}
