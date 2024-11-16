c02fc923cbb7e65790f860927459d5c545cf5697
Ervis Trupja <ervis@bettertrucks.com>
2024-11-16 17:45:18 +0100

    implemented posts service

--- CircleApp.Data/Services/PostsService.cs
+++ CircleApp.Data/Services/PostsService.cs
@@ -1,5 +1,6 @@
 ﻿using CircleApp.Data.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,44 +17,138 @@ namespace CircleApp.Data.Services
             _context = context;
         }
 
-        public Task AddPostCommentAsync(Comment comment)
+        public async Task<List<Post>> GetAllPostsAsync(int loggedInUserId)
         {
-            throw new NotImplementedException();
+            var allPosts = await _context.Posts
+                .Where(n => (!n.IsPrivate || n.UserId == loggedInUserId) && n.Reports.Count < 5 && !n.IsDeleted)
+                .Include(n => n.User)
+                .Include(n => n.Likes)
+                .Include(n => n.Favorites)
+                .Include(n => n.Comments).ThenInclude(n => n.User)
+                .Include(n => n.Reports)
+                .OrderByDescending(n => n.DateCreated)
+                .ToListAsync();
+
+            return allPosts;
         }
 
-        public Task<Post> CreatePostAsync(Post post, IFormFile Image)
+        public async Task AddPostCommentAsync(Comment comment)
         {
-            throw new NotImplementedException();
+            await _context.Comments.AddAsync(comment);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<List<Post>> GetAllPostsAsync(int loggedInUserId)
+        public async Task<Post> CreatePostAsync(Post post, IFormFile image)
         {
-            throw new NotImplementedException();
+            //Check and save the image
+            if (image != null && image.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                if (image.ContentType.Contains("image"))
+                {
+                    string rootFolderPathImages = Path.Combine(rootFolderPath, "images/posts");
+                    Directory.CreateDirectory(rootFolderPathImages);
+
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                    string filePath = Path.Combine(rootFolderPathImages, fileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                        await image.CopyToAsync(stream);
+
+                    //Set the URL to the newPost object
+                    post.ImageUrl = "/images/posts/" + fileName;
+                }
+            }
+
+            await _context.Posts.AddAsync(post);
+            await _context.SaveChangesAsync();
+
+            return post;
         }
 
-        public Task RemovePostAsync(int postId)
+        public async Task RemovePostAsync(int postId)
         {
-            throw new NotImplementedException();
+            var postDb = await _context.Posts.FirstOrDefaultAsync(n => n.Id == postId);
+
+            if (postDb != null)
+            {
+                //_context.Posts.Remove(postDb);
+                postDb.IsDeleted = true;
+                _context.Posts.Update(postDb);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        public Task RemovePostCommentAsync(int commentId)
+        public async Task RemovePostCommentAsync(int commentId)
         {
-            throw new NotImplementedException();
+            var commentDb = _context.Comments.FirstOrDefault(n => n.Id == commentId);
+
+            if(commentDb != null)
+            {
+                _context.Comments.Remove(commentDb);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        public Task ReportPostAsync(int postId, int userId)
+        public async Task ReportPostAsync(int postId, int userId)
         {
-            throw new NotImplementedException();
+            var newReport = new Report()
+            {
+                PostId = postId,
+                UserId = userId,
+                DateCreated = DateTime.UtcNow
+            };
+
+            await _context.Reports.AddAsync(newReport);
+            await _context.SaveChangesAsync();
         }
 
-        public Task TogglePostFavoriteAsync(int postId, int userId)
+        public async Task TogglePostFavoriteAsync(int postId, int userId)
         {
-            throw new NotImplementedException();
+            //check if user has already favorited the post
+            var favorite = await _context.Favorites
+                .Where(l => l.PostId == postId && l.UserId == userId)
+                .FirstOrDefaultAsync();
+
+            if (favorite != null)
+            {
+                _context.Favorites.Remove(favorite);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newFavorite = new Favorite()
+                {
+                    PostId = postId,
+                    UserId = userId
+                };
+                await _context.Favorites.AddAsync(newFavorite);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        public Task TogglePostLikeAsync(int postId, int userId)
+        public async Task TogglePostLikeAsync(int postId, int userId)
         {
-            throw new NotImplementedException();
+            //check if user has already liked the post
+            var like = await _context.Likes
+                .Where(l => l.PostId == postId && l.UserId == userId)
+                .FirstOrDefaultAsync();
+
+            if (like != null)
+            {
+                _context.Likes.Remove(like);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                var newLike = new Like()
+                {
+                    PostId = postId,
+                    UserId = userId
+                };
+                await _context.Likes.AddAsync(newLike);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -53,26 +53,7 @@ namespace CircleApp.Controllers
                 UserId = loggedInUser
             };
 
-            //Check and save the image
-            if (post.Image != null && post.Image.Length > 0)
-            {
-                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
-                if (post.Image.ContentType.Contains("image"))
-                {
-                    string rootFolderPathImages = Path.Combine(rootFolderPath, "images/posts");
-                    Directory.CreateDirectory(rootFolderPathImages);
-
-                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(post.Image.FileName);
-                    string filePath = Path.Combine(rootFolderPathImages, fileName);
-
-                    using (var stream = new FileStream(filePath, FileMode.Create))
-                        await post.Image.CopyToAsync(stream);
-
-                    //Set the URL to the newPost object
-                    newPost.ImageUrl = "/images/posts/" + fileName;
-                }
-            }
-
+            
             //Add the post to the database
             await _context.Posts.AddAsync(newPost);
             await _context.SaveChangesAsync();
