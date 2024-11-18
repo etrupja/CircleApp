e12f0316c2a493bb3d076eca759fbb0f9b364fab
Ervis Trupja <ervis@bettertrucks.com>
2024-11-18 21:15:38 +0100

    added favorites controller and action and view

---
+++ CircleApp/Controllers/FavoritesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Controllers
+{
+    public class FavoritesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
