ec269b5142d7a35b865a755566678dc9ef0b56e0
Ervis Trupja <ervis@bettertrucks.com>
2024-11-24 20:14:07 +0100

    added user settings controller and action

---
+++ CircleApp/Controllers/SettingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
