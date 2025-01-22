f7bb2d4960d5aa0a1fa018b477aa9e27ef65db1b
Ervis Trupja <ervis@bettertrucks.com>
2025-01-22 20:51:40 +0100

    disabled default passowrd requirments

--- CircleApp/Program.cs
+++ CircleApp/Program.cs
@@ -24,7 +24,15 @@ builder.Services.AddScoped<IUsersService, UsersService>();
 
 
 //Identity configuration
-builder.Services.AddIdentity<User, IdentityRole<int>>()
+builder.Services.AddIdentity<User, IdentityRole<int>>(options =>
+{
+    //Password settings
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 4;
+})
     .AddEntityFrameworkStores<AppDbContext>()
     .AddDefaultTokenProviders();
 
