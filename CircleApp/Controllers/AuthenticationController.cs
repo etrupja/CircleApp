876ba2eb8caafa6469013f98a9a05809972a4cea
Ervis Trupja <ervis@bettertrucks.com>
2025-01-22 19:57:52 +0100

    registering a new user

--- CircleApp/Controllers/AuthenticationController.cs
+++ CircleApp/Controllers/AuthenticationController.cs
@@ -1,9 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CircleApp.Data.Helpers.Constants;
+using CircleApp.Data.Models;
+using CircleApp.ViewModels.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CircleApp.Controllers
 {
     public class AuthenticationController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        public AuthenticationController(UserManager<User> userManager, 
+            SignInManager<User> signInManager) 
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public async Task<IActionResult> Login()
         {
             return View();
@@ -11,6 +24,29 @@ namespace CircleApp.Controllers
 
         public async Task<IActionResult> Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            var newUser = new User()
+            {
+                FullName = $"{registerVM.FirstName} {registerVM.LastName}",
+                Email = registerVM.Email,
+                UserName = registerVM.Email
+            };
+
+            var result = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if(result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, AppRoles.User);
+
+                await _signInManager.SignInAsync(newUser, isPersistent: false);
+                return RedirectToAction("Index", "Home");
+            }
+
             return View();
         }
     }
---
+++ CircleApp/ViewModels/Authentication/RegisterVM.cs
@@ -0,0 +1,11 @@
+﻿namespace CircleApp.ViewModels.Authentication
+{
+    public class RegisterVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
