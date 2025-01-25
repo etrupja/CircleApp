a5d7c53493d39ef69acfe9ee9aee98f97342ea09
Ervis Trupja <ervistrupja@outlook.com>
2025-01-25 20:24:13 +0100

    added the redirect links and also the users controller

---
+++ CircleApp/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using CircleApp.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.Controllers
+{
+    public class UsersController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(string userId)
+        {
+            return View();
+        }
+    }
+}
