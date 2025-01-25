1431b5f9ee2fe51de38aafaa77ad8981d4986a3a
Ervis Trupja <ervistrupja@outlook.com>
2025-01-25 18:21:32 +0100

    configured google login

--- CircleApp/Program.cs
+++ CircleApp/Program.cs
@@ -43,12 +43,13 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.AccessDeniedPath = "/Authentication/AccessDenied";
 });
 
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//    .AddCookie(options =>
-//    {
-//        options.LoginPath = "/Authentication/Login";
-//        options.AccessDeniedPath = "/Authentication/AccessDenied";
-//    });
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddGoogle(options =>
+    {
+        options.ClientId = builder.Configuration["Auth:Google:ClientId"] ?? "";
+        options.ClientSecret = builder.Configuration["Auth:Google:ClientSecret"] ?? "";
+        options.CallbackPath = "/signin-google";
+    });
 
 builder.Services.AddAuthorization();
 
