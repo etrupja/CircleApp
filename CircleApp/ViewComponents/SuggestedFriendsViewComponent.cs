6b2c241fa48a5c5fda09eb853af467ad2f6a712e
Ervis Trupja <ervistrupja@outlook.com>
2025-01-29 21:27:33 +0100

    addd new viewcomponent for suggested friends

---
+++ CircleApp/ViewComponents/SuggestedFriendsViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CircleApp.ViewComponents
+{
+    public class SuggestedFriendsViewComponent:ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
