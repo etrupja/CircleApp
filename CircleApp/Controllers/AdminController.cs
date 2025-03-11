74b03b2caeb1ffd307b3076bec20a8b7dac07fc5
Ervis Trupja <ervistrupja@outlook.com>
2025-03-11 22:46:23 +0100

    created admin dashboard

---
+++ CircleApp/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using CircleApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace CircleApp.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly AppDbContext _context;
+        public AdminController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allPosts = await _context.Posts.Include(n => n.User).ToListAsync();
+
+            return View(allPosts);
+        }
+    }
+}
